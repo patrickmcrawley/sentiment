a9b5effa8f00727b430652da42844f08c8143fe1
patrickmcrawley <patrick.m.crawley@gmail.com>
2020-04-14 00:13:04 -0400

    a

--- sint.py
+++ sint.py
@@ -1,18 +1,18 @@
 import quandl
 import pandas
 import datetime
+import get_sp500 as sp
 
 #quandl.ApiConfig.api_key = 'kTx7XssV5LbHw4AmPyvf'
 #print(quandl.get("FINRA/FNYX_AAPL", authtoken="kTx7XssV5LbHw4AmPyvf"))
 
 
-list = ['AAPL', 'NFLX']
-for x in list:
-
+tickers = sp.get_sp500()
+for x in tickers:
     #print('FINRA/FORF_' + x)
     sec = str('FINRA/FNSQ_' + x)
     print(sec)
     quandl.ApiConfig.api_key = 'kTx7XssV5LbHw4AmPyvf'
-    print(quandl.get(sec))
+    print(quandl.get(sec).head())
 
 
