96f0f433c7cac2c657147072efee02119eacd88a
Ryan <ryanfitz514@gmail.com>
2020-04-14 02:08:21 -0400

    - added to sint.py to print out short volume and the ticker.
    - We needed to have the individual calls all go into a joined dataframe.

--- sint.py
+++ sint.py
@@ -1,18 +1,27 @@
 import quandl
-import pandas
+import pandas as pd
 import datetime
+
+from pandas import DataFrame
+
 import get_sp500 as sp
 
 #quandl.ApiConfig.api_key = 'kTx7XssV5LbHw4AmPyvf'
 #print(quandl.get("FINRA/FNYX_AAPL", authtoken="kTx7XssV5LbHw4AmPyvf"))
 
-
+append_df = pd.DataFrame()
 tickers = sp.get_sp500()
 for x in tickers:
     #print('FINRA/FORF_' + x)
     sec = str('FINRA/FNSQ_' + x)
-    print(sec)
     quandl.ApiConfig.api_key = 'kTx7XssV5LbHw4AmPyvf'
-    print(quandl.get(sec).head())
+    newest1 = quandl.get(sec).tail(1)
+    newest1['Ticker'] = x
+    DataFrame = newest1.drop(['ShortExemptVolume', 'TotalVolume'], axis=1)
+    print(DataFrame)
+    appended_df = append_df.append(DataFrame)
+
+
+
 
 
