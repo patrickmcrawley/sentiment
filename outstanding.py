29ca472e73b27fc2b95a9de79da7f5a32463a80d
patrickmcrawley <patrick.m.crawley@gmail.com>
2020-04-18 02:55:12 -0400

    a

---
+++ outstanding.py
@@ -0,0 +1,74 @@
+from yahooquery import Ticker
+import get_sp500 as sp
+import pandas as pd
+import os as os
+import sqlite3
+
+
+
+def share_values():
+    conn = sqlite3.connect('SP500.db')
+    c = conn.cursor()
+    c.execute('''DROP TABLE IF EXISTS key_stats''')
+    conn.commit()
+    conn.close()
+    tickers = sp.get_sp500()
+    with sqlite3.connect('SP500.db') as conn:
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS key_stats (tickers TEXT, shares_outstanding INTEGER, short_interest INTEGER, float INTEGER, market_cap INTEGER, industry TEXT)''')
+        sos_values = 0
+        short_int = 0
+        float = 0
+        mcap = 0
+        industry = ''
+        for x in tickers:
+            ticks = Ticker(x)
+            print('getting key statistics {}'.format(x))
+            stockvalues = ticks.key_stats
+            for key, value in stockvalues.items():
+                try:
+                    sos_values = value['sharesOutstanding']
+                    short_int = value['sharesShort']
+                    float = value['floatShares']
+                except:
+                    print('error stockvalues {}'.format(x))
+                    continue
+            details = ticks.summary_detail
+            for key, value in details.items():
+                try:
+                    mcap = value['marketCap']
+                except:
+                    print('error market cap {}'.format(x))
+                    continue
+            profile = ticks.summary_profile
+            for key, value in profile.items():
+                try:
+                    industry = value['industry']
+                except:
+                    print('industry error {}'.format(x))
+                    continue
+        print('We are writing to the DB now')
+        c.execute('''INSERT INTO key_stats (tickers, shares_outstanding, short_interest, float, market_cap, industry) VALUES (?, ?, ?, ?, ?, ?)''', (str(x), int(sos_values), int(short_int), int(float), int(mcap), str(industry)))
+        conn.commit()
+        c.execute('''SELECT * FROM key_stats''')
+        result = c.fetchall()
+        return result
+
+
+share_values()
+
+# def write_to_sql():
+#     share_values()
+
+
+
+
+
+
+
+
+
+
+
+
+
